9e7b1c8f62ab33ed859f1d060e376d904ec456e7
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-20 00:18:51 +0530

    Logo Section Component Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
+import LogoSection from "./components/LogoSection";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <LogoSection />
     </div>
   );
 }
