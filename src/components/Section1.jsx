c6b8aceac3ea21b6b0021ca2dd72b7e142726fe2
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-19 15:56:40 +0530

    Section 1 Added

---
+++ src/components/Section1.jsx
@@ -0,0 +1,25 @@
+import "../style/Section1.css";
+import triangleImg from "../assets/triangle.png";
+import halfcircleImg from "../assets/halfcircle.png";
+
+function Section1() {
+  return (
+    <div className="section1">
+      <div className="section1-header">Build advanced chatbots visually</div>
+      <div className="section1-description">
+        Typebot gives you powerful blocks to create unique chat experiences.
+        Embed them anywhere on your web/mobile apps and start collecting results
+        like magic.
+      </div>
+      <div>
+        <button className="create-formBit-btn">
+          Create a FormBot for free
+        </button>
+      </div>
+      <img className="triangle-img" src={triangleImg} alt="triangle" />
+      <img className="halfcircle-img" src={halfcircleImg} alt="halfcircle" />
+    </div>
+  );
+}
+
+export default Section1;
