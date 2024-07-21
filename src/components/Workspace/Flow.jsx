e5b525a2ecf65f385b668f12efb46e52f8d1a9a4
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-22 00:20:55 +0530

    Flow Component Added for Workspace

---
+++ src/components/Workspace/Flow.jsx
@@ -0,0 +1,88 @@
+import "../../style/Workspace/Flow.css";
+import start from "../../assets/Workspace/Flow/start.png";
+
+// Bubble Icons
+import textBubble from "../../assets/Workspace/Flow/Bubbles/text.png";
+import image from "../../assets/Workspace/Flow/Bubbles/image.png";
+import video from "../../assets/Workspace/Flow/Bubbles/video.png";
+import gif from "../../assets/Workspace/Flow/Bubbles/gif.png";
+
+// input Icons
+import textInput from "../../assets/Workspace/Flow/Inputs/text.png";
+import numberInput from "../../assets/Workspace/Flow/Inputs/number.png";
+import emailInput from "../../assets/Workspace/Flow/Inputs/email.png";
+import phoneInput from "../../assets/Workspace/Flow/Inputs/phone.png";
+import dateInput from "../../assets/Workspace/Flow/Inputs/date.png";
+import ratingInput from "../../assets/Workspace/Flow/Inputs/rating.png";
+import buttonInput from "../../assets/Workspace/Flow/Inputs/button.png";
+
+function Flow() {
+  return (
+    <div className="flow">
+      <div className="flow-items">
+        <div className="flow-container">
+          <div className="sub-header sub-header-bubbles">Bubbles</div>
+          <div className="bubbles">
+            <div className="bubble">
+              <img src={textBubble} alt="Text Bubble" />
+              Text
+            </div>
+            <div className="bubble">
+              <img src={image} alt="Images Bubble" />
+              Image
+            </div>
+            <div className="bubble">
+              <img src={video} alt="Video Bubble" />
+              Video
+            </div>
+            <div className="bubble">
+              <img src={gif} alt="GIF Bubble" />
+              GIF
+            </div>
+          </div>
+
+          <div className="sub-header">Inputs</div>
+          <div className="bubbles">
+            <div className="bubble">
+              <img src={textInput} alt="Text Input" />
+              Text
+            </div>
+            <div className="bubble">
+              <img src={numberInput} alt="Number Input" />
+              Number
+            </div>
+            <div className="bubble">
+              <img src={emailInput} alt="Email Input" />
+              Email
+            </div>
+            <div className="bubble">
+              <img src={phoneInput} alt="Phone Input" />
+              Phone
+            </div>
+            <div className="bubble">
+              <img src={dateInput} alt="Date Input" />
+              Date
+            </div>
+            <div className="bubble">
+              <img src={ratingInput} alt="Rating Input" />
+              Rating
+            </div>
+            <div className="bubble">
+              <img src={buttonInput} alt="Button Input" />
+              Button
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flowchart-container">
+        <div className="start-header">
+          <img src={start} alt="start" />
+          Start
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Flow;
--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -1,10 +1,12 @@
 import "../style/Workspace/Workspace.css";
 import WorkspaceNavBar from "../components/Workspace/WorkspaceNavBar.jsx";
+import Flow from "../components/Workspace/Flow.jsx";
 
 function Workspace() {
   return (
     <div className="workspace">
       <WorkspaceNavBar />
+      <Flow />
     </div>
   );
 }
