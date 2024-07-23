3ccea3cd3bb5fc6f860f14300b049c403bdfbb84
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-23 17:41:29 +0530

    Response Component Added

---
+++ src/components/Workspace/Response.jsx
@@ -0,0 +1,11 @@
+import "../../style/Workspace/Response.css";
+
+function Response() {
+  return (
+    <div className='response'>
+      No Response yet collected
+    </div>
+  )
+}
+
+export default Response
--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -1,14 +1,16 @@
 import "../style/Workspace/Workspace.css";
 import WorkspaceNavBar from "../components/Workspace/WorkspaceNavBar.jsx";
 // import Flow from "../components/Workspace/Flow.jsx";
-import Theme from "../components/Workspace/Theme.jsx";
+// import Theme from "../components/Workspace/Theme.jsx";
+import Response from "../components/Workspace/Response.jsx";
 
 function Workspace() {
   return (
     <div className="workspace">
       <WorkspaceNavBar />
       {/* <Flow /> */}
-      <Theme />
+      {/* <Theme /> */}
+      <Response />
     </div>
   );
 }
