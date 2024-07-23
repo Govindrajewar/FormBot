cb96388a8d03beea8cafd406d547cbb49dbfa2a1
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-23 11:35:44 +0530

    Light, Dark, Blue Theme functionality added

--- src/components/Workspace/Theme.jsx
+++ src/components/Workspace/Theme.jsx
@@ -5,14 +5,36 @@ import dark from "../../assets/Workspace/Theme/dark.png";
 import blue from "../../assets/Workspace/Theme/blue.png";
 
 function Theme() {
+  const addLightTheme = () => {
+    document.getElementById("themeSettingsId").classList.add("light-theme");
+    document
+      .getElementById("themeSettingsId")
+      .classList.remove("dark-theme", "blue-theme");
+  };
+
+  const addDarkTheme = () => {
+    document.getElementById("themeSettingsId").classList.add("dark-theme");
+    document
+      .getElementById("themeSettingsId")
+      .classList.remove("light-theme", "blue-theme");
+  };
+
+  const addBlueTheme = () => {
+    document.getElementById("themeSettingsId").classList.add("blue-theme");
+    document
+      .getElementById("themeSettingsId")
+      .classList.remove("dark-theme", "light-theme");
+  };
+
   return (
     <div className="theme">
       <div className="theme-container">
         <div className="theme-header">Customize the theme</div>
-        <img src={light} alt="light" />
-        <img src={dark} alt="dark" />
-        <img src={blue} alt="blue" />
+        <img src={light} alt="light" onClick={addLightTheme} />
+        <img src={dark} alt="dark" onClick={addDarkTheme} />
+        <img src={blue} alt="blue" onClick={addBlueTheme} />
       </div>
+      <div className="theme-setting light-theme" id="themeSettingsId"></div>
     </div>
   );
 }
