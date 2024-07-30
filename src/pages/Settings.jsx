c695fe5ed1a9fec5fc88ffb3902ebe896a05b6f7
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-31 00:18:25 +0530

    Added toggle for view to show password

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../style/Settings/Settings.css";
 import lock from "../assets/Settings/lock.png";
 import logout from "../assets/Settings/logout.png";
@@ -11,6 +12,9 @@ function Settings() {
   const handleLogout = () => {
     navigate("/");
   };
+
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showOldPassword, setShowOldPassword] = useState(false);
 
   return (
     <div className="settings">
@@ -26,13 +30,29 @@ function Settings() {
       </div>
       <div className="data-container">
         <img src={lock} alt="lock" />
-        <input type="password" placeholder="Old Password" />
-        <img src={view} alt="view" id="viewId" />
+        <input
+          type={showOldPassword ? "text" : "password"}
+          placeholder="Old Password"
+        />
+        <img
+          src={view}
+          alt="view"
+          id="viewId"
+          onClick={() => setShowOldPassword(!showOldPassword)}
+        />
       </div>
       <div className="data-container">
         <img src={lock} alt="lock" />
-        <input type="password" placeholder="New Password" />
-        <img src={view} alt="view" id="viewId" />
+        <input
+          type={showNewPassword ? "text" : "password"}
+          placeholder="New Password"
+        />
+        <img
+          src={view}
+          alt="view"
+          id="viewId"
+          onClick={() => setShowNewPassword(!showNewPassword)}
+        />
       </div>
 
       <button type="button" id="update-btn">
