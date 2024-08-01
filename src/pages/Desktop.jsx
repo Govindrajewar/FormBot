e443ff82e9b56185d0bf05fc772bba7cf5d916d8
Govind Rajewar <abhirajewar87@gmail.com>
2024-08-01 14:16:42 +0530

    Desktop: Fetching data from database depending on form Name

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -1,11 +1,67 @@
 import "../style/Desktop/Desktop.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import icon from "../../src/assets/Workspace/Theme/icon.png";
+import send from "../assets/Desktop/send.png";
 
 function Desktop() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // TODO: Move this function to "api/fetchedData"
+    // Fetching data from the backend API
+    axios
+      .get("http://localhost:4001/formdata")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  const filteredData = data.filter((form) => form.formName === "Introduction");
+
   return (
     <div className="desktop">
-      Component to display new created form
+      <div className="chat-container">
+        {filteredData.length > 0 ? (
+          filteredData.map((form) =>
+            form.itemList.map((item, index) => (
+              <div
+                key={index}
+                className={`data-container ${
+                  item.type === "textInput" ? "right" : "left"
+                }`}
+              >
+                {item.type !== "textInput" && (
+                  <>
+                    <img src={icon} alt="icon" className="data-icon" />
+                    <p className="chat-bubble">{item.value}</p>
+                  </>
+                )}
+                {item.type === "textInput" && (
+                  <div className="text-input-container">
+                    <span className="text-input-label">{item.value}</span>
+                    <input
+                      type="text"
+                      placeholder="Enter your text"
+                      className="text-input"
+                    />
+                    <button className="submit-button">
+                      <img src={send} alt="Send" />
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))
+          )
+        ) : (
+          <p>No items available for "Introduction"</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Desktop
+export default Desktop;
